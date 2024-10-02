1f44d508ef32fc2db42866d1083edcc29972a7e3
Mayur Tekale <mayurtekale7@gmail.com>
2024-10-02 18:46:09 +0530

    Created file

---
+++ drum-kit/first.js
@@ -0,0 +1,18 @@
+window.addEventListener('keydown', (e) =>{
+    const audio = document.querySelector(`audio[data-key ="${e.keyCode}"]`);
+    const key =  document.querySelector(`key[data-key ="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime =0; 
+    audio.play();
+    key.classList.add('playing');
+
+});
+
+function removeTransition(e){
+    console.log(e);
+    
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
